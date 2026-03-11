78e93cadcf9aba8c708a61da82047b19d4fcd9d9
Kullu Ashok <kulluashok14@gmail.com>
2026-03-11 20:00:29 +0530

    all changes done at 8

--- script.js
+++ script.js
@@ -119,3 +119,43 @@ document.querySelectorAll('.cert-pdf').forEach(link => {
     });
 });
   
+
+
+//  form filling submission
+const form = document.getElementById("contact-form");
+const status = document.getElementById("form-status");
+
+form.addEventListener("submit", async function (e) {
+
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    try {
+
+        const response = await fetch(form.action, {
+            method: form.method,
+            body: data,
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+
+            status.innerHTML = " Message sent successfully!";
+            form.reset();
+
+        } else {
+
+            status.innerHTML = " Oops! Something went wrong.";
+
+        }
+
+    } catch (error) {
+
+        status.innerHTML = " Network error.";
+
+    }
+
+});
